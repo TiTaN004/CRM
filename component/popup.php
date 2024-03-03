a7d517a3c30749b82ea9d30229714df925c62c2e
TiTaN004 <mananrathod214@gmail.com>
2024-03-03 13:28:33 +0530

    03/03/2024

---
+++ component/popup.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script>
+   document.addEventListener('DOMContentLoaded', function () {
+       var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+       myModal.show();
+   });
+</script>
+<!-- Favicon -->
+<link rel="shortcut icon" href="../assets/images/favicon.ico" />
+      
+      <link rel="stylesheet" href="../assets/css/backend-plugin.min.css">
+      <link rel="stylesheet" href="../assets/css/backend.css?v=1.0.0"> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="../operation/operation.php" method="post">
+      <div class="form-group">
+                    <label for="fname">First Name</label>
+                    <input type="text" class="form-control" name="fname" id="fname" required>
+                </div>
+                <div class="form-group">
+                    <label for="lname">Last Name</label>
+                    <input type="text" class="form-control" name="lname" id="lname" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="Phone">Phone</label>
+                    <input type="number" class="form-control" name="phn" id="Phone" required>
+                </div>
+                <div class="form-group">
+                    <label for="state">State</label>
+                    <input type="text" class="form-control" name="state" id="state" required>
+                </div>
+                <div class="form-group">
+                    <label for="Inquiry">Inquiry</label>
+                    <input type="text" class="form-control" name="inquiry" id="inquiry"  required>
+                </div>
+      </div>
+      <div class="modal-footer">
+        <a href="../"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></a>
+        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<?php
+
+
+
+?>
