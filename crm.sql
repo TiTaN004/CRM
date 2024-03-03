a1fe1099ac0f5c3d9284e0c931d9eeac557859ae
TiTaN004 <mananrathod214@gmail.com>
2024-03-03 13:29:53 +0530

    03/03/2024

---
+++ crm.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2024 at 08:59 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lead`
+--
+
+CREATE TABLE `lead` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` int(10) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `source` varchar(50) NOT NULL,
+  `Inquiry` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `lead`
+--
+
+INSERT INTO `lead` (`id`, `fname`, `lname`, `email`, `phone`, `state`, `source`, `Inquiry`, `status`) VALUES
+(1, 'manan', 'rathod', 'mananrathod214@gmail.com', 12364789, 'ads', 'asdf', 'dsga', 'Ordered'),
+(2, 'tanan', 'rathod', 'mananrathod214@gmail.com', 123456789, 'guj', 'indiamart', 'asdf', 'ordered'),
+(3, 'harshil', 'patel', 'harshil@gmail.com', 12345678, 'guj', 'l;sdkfj', 'la;jdf', 'ordered'),
+(4, 'manan', 'rathod', 'mananrathod214@gmail.com', 12364789, 'ads', 'asdf', 'dsga', 'not-interested'),
+(5, 'tanan', 'rathod', 'mananrathod214@gmail.com', 123456789, 'guj', 'indiamart', 'asdf', 'ordered'),
+(6, 'harshil', 'patel', 'harshil@gmail.com', 12345678, 'guj', 'l;sdkfj', 'la;jdf', 'ordered'),
+(7, 'manan', 'rathod', 'mananrathod214@gmail.com', 12364789, 'ads', 'asdf', 'dsga', 'not-interested'),
+(8, 'tanan', 'rathod', 'mananrathod214@gmail.com', 123456789, 'guj', 'indiamart', 'asdf', 'ordered'),
+(9, 'harshil', 'patel', 'harshil@gmail.com', 12345678, 'guj', 'l;sdkfj', 'la;jdf', 'ordered'),
+(10, 'manan', 'rathod', 'mananrathod214@gmail.com', 12364789, 'ads', 'asdf', 'dsga', 'not-interested'),
+(11, 'tanan', 'rathod', 'mananrathod214@gmail.com', 123456789, 'guj', 'indiamart', 'asdf', 'ordered'),
+(12, 'harshil', 'patel', 'harshil@gmail.com', 12345678, 'guj', 'l;sdkfj', 'la;jdf', 'ordered'),
+(13, 'xyz', 'abc', 'admin@gmail.com', 11, 'guj', 'l;sdkfj', 'la;jdf', 'Not interested'),
+(14, 'xyz', 'abc', 'admin@gmail.com', 11, 'guj', 'l;sdkfj', 'la;jdf', 'Not interested'),
+(19, '0', '0', '0@gmail.com', 123456789, 'guj', 'a', 'b', 'Ordered'),
+(20, 'jeet', 'patel', 'jeet@gmail.com', 1232456789, 'guj', 'website', 'iphone', 'inquiry'),
+(21, 'jeet', 'patel', 'jeet@gmail.com', 1232456789, 'guj', 'website', 'iphone', 'inquiry'),
+(22, 'harshil', 'patel', 'harshil@gmail.com', 123456789, 'guj', 'website', 'iphone', 'inquiry');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `img_url` varchar(1000) NOT NULL,
+  `description` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `img_url`, `description`) VALUES
+(1, 'Iphone', 'iphone.jpg', 'iphone 15 pro, black color, 128gb storage'),
+(2, 'Ipad', 'ipad.jpg', 'Ipad 10th Gen, color blue, 64gb'),
+(3, 'MacBook Pro', 'macbook pro.jpg', 'MacBook Pro Max, color space gray, 512gb ssd'),
+(4, 'MacBook Pro', 'macbook pro.jpg', 'MacBook Pro, Space Gray, 512 ssd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `no` int(11) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pass` varchar(8) NOT NULL,
+  `num` int(10) NOT NULL,
+  `count` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`no`, `uname`, `email`, `pass`, `num`, `count`) VALUES
+(1, 'admin', 'admin@gmail.com', 'admin', 0, ''),
+(2, 'manan', 'mananrathod214@gmail.com', '12345678', 0, ''),
+(3, 'manan rathod', 'mananrathod45@gmail.com', 'manan', 2147483647, 'india'),
+(4, 'nishit', 'nishit@gmail.com', 'nishit', 1234567890, 'india'),
+(5, 'janan', 'janan@gmail.com', 'janan', 123456789, 'india'),
+(6, 'tanan', 'tanan@gmail.com', 'tanan', 123456789, 'india'),
+(8, 'harshil', 'harshil1@gmail.com', 'harshil', 12334444, 'india'),
+(9, 'manan', 'mananrathod1@gmail.com', 'manan', 12345678, 'india'),
+(10, 'manan', 'mananrathod12@gmail.com', 'manan', 12345678, 'india');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lead`
+--
+ALTER TABLE `lead`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lead`
+--
+ALTER TABLE `lead`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
